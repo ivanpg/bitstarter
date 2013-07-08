94027a919a7e7bbfadefcb5d0f9c7466999e0c5f
Ivan Posilio <ivanpgcs@gmail.com>
2013-07-08 18:30:58 +0000

    Updated web.js with index.html

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var index = fs.readFileSync("index.html");
+var buffer = new Buffer(index,"utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!'); 
-});
+  response.send(buffer.toString());
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
